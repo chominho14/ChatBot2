254f718909876dcccc06e4740fa025cc10db16be
chominho14 <chominho14@naver.com>
2022-07-11 10:49:39 +0900

    v1.0.0(Chatbot - basic)

--- src/controllers/messageControllers.js
+++ src/controllers/messageControllers.js
@@ -16,49 +16,18 @@ const sessionClient = new dialogflow.SessionsClient({
 });
 const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 
-let fakeMessages = [
-  { username: "minho", text: "how are u?" },
-  { username: "bot", text: "No andswer" },
-];
-
-export const handleHome = (req, res) => {
-  return res.render("home", { pageTitle: "Home", fakeMessages });
+export const handleHome = async (req, res) => {
+  const messages = await Message.find({});
+  return res.render("home", { pageTitle: "Home", messages });
 };
 
 export const postMessage = async (req, res) => {
-  const { username, text } = req.body;
-  console.log(req.body);
-  const request = {
-    session: sessionPath,
-    queryInput: {
-      text: {
-        // The query to send to the dialogflow agent
-        text: req.body.text,
-        // The language used by the client (en-US)
-        languageCode: languageCode,
-      },
-    },
-  };
-
-  // Send request and log result
-  const responses = await sessionClient.detectIntent(request);
-  console.log("Detected intent");
-  const result = responses[0].queryResult;
-  console.log(`  Query: ${result.queryText}`);
-  console.log(`  Response: ${result.fulfillmentText}`);
-
-  // res.send(result);
-
-  const newMessages = {
-    username,
+  const { name, text } = req.body;
+  await Message.create({
+    name,
     text,
-  };
-  fakeMessages.push(newMessages);
-  return res.render("home", { pageTitle: "Home", fakeMessages });
-};
+  });
 
-export const textQuery = async (req, res) => {
-  console.log(req.body);
   const request = {
     session: sessionPath,
     queryInput: {
@@ -78,28 +47,10 @@ export const textQuery = async (req, res) => {
   console.log(`  Query: ${result.queryText}`);
   console.log(`  Response: ${result.fulfillmentText}`);
 
-  res.send(result);
-};
+  await Message.create({
+    name: "bot",
+    text: result.fulfillmentText,
+  });
 
-export const eventQuery = async (req, res) => {
-  const request = {
-    session: sessionPath,
-    queryInput: {
-      event: {
-        // The query to send to the dialogflow agent
-        name: req.body.event,
-        // The language used by the client (en-US)
-        languageCode: languageCode,
-      },
-    },
-  };
-
-  // Send request and log result
-  const responses = await sessionClient.detectIntent(request);
-  console.log("Detected intent");
-  const result = responses[0].queryResult;
-  console.log(`  Query: ${result.queryText}`);
-  console.log(`  Response: ${result.fulfillmentText}`);
-
-  res.send(result);
+  return res.redirect("/");
 };
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,16 +1,9 @@
 import express from "express";
-import {
-  handleHome,
-  postMessage,
-  textQuery,
-  eventQuery,
-} from "../controllers/messageControllers";
+import { handleHome, postMessage } from "../controllers/messageControllers";
 
 const homeRouter = express.Router();
 
 homeRouter.get("/", handleHome);
 homeRouter.post("/", postMessage);
-homeRouter.post("/textQuery", textQuery);
-homeRouter.post("/eventQuery", eventQuery);
 
 export default homeRouter;
